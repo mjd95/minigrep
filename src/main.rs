de08bbef302db1c200eae0fe6d95db6ca7b72093
Martin Dickson <martin.dickson@protonmail.com>
2018-09-23 15:46:29 +0100

    accepts command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("searching for {}", query);
+    println!("in file {}", filename);
 }
